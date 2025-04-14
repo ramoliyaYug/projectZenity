2b2e0b01f5c8fcd5e19ba3cbda3773adf0d38336
ramoliyaYug <mastermind57369@gmail.com>
2025-04-14 16:49:03 +0530

    added the splash screen and the logo for the app

---
+++ app/src/main/java/com/example/projectzenity/view/Splash_Screen.kt
@@ -0,0 +1,28 @@
+package com.example.projectzenity.view
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.projectzenity.MainActivity
+import com.example.projectzenity.R
+import com.example.projectzenity.databinding.ActivitySplashScreenBinding
+import java.util.logging.Handler
+
+class Splash_Screen : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashScreenBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        android.os.Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
