5f10a9da4f27d77b9504301a2f951e5f9f99e09b
ramoliyaYug <mastermind57369@gmail.com>
2025-04-15 12:00:13 +0530

    added fade in animation in the splash screen

--- app/src/main/java/com/example/projectzenity/view/Splash_Screen.kt
+++ app/src/main/java/com/example/projectzenity/view/Splash_Screen.kt
@@ -2,24 +2,28 @@ package com.example.projectzenity.view
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.os.Looper
-import androidx.activity.enableEdgeToEdge
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.projectzenity.MainActivity
 import com.example.projectzenity.R
 import com.example.projectzenity.databinding.ActivitySplashScreenBinding
-import java.util.logging.Handler
 
 class Splash_Screen : AppCompatActivity() {
     private lateinit var binding: ActivitySplashScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        android.os.Handler(Looper.getMainLooper()).postDelayed({
+        // Apply fade-in animation to logo
+        val fadeIn = AnimationUtils.loadAnimation(this, R.anim.splash_screen_logo_fade_in)
+        binding.logoImage.startAnimation(fadeIn)
+
+        // Move to next activity after delay
+        Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
